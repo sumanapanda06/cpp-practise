6f7b592675dd86e8db517c4ef32f12c7ca5a8402
Sumana Panda <manjupanda42@gmail.com>
2026-05-07 16:16:15 +0530

    added sum of two numbers program

---
+++ basics/sum-of-two-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n1;
+    cout<<"Enter first number: ";
+    cin>>n1;
+    int n2;
+    cout<<"Enter second number";
+    cin>>n2;
+
+    int sum=n1+n2;
+    cout<<sum;
+    return 0;
+}
