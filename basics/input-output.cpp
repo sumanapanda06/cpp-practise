59a0face7cb8a4461fd26ce7d14ece7c59d5c93c
Sumana Panda <manjupanda42@gmail.com>
2026-05-07 15:41:29 +0530

    Added input output program

---
+++ basics/input-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int num;
+    cout<<"Enter int value: ";
+    cin>>num;
+    double number;
+    cout<<"Enter double value: ";
+    cin>>number;
+    float value;
+    cout<<"Enter float value: ";
+    cin>>value;
+    char alph;
+    cout<<"Enter char value: ";
+    cin>>alph;
+
+    cout<<num<<endl<<number<<endl<<value<<endl<<alph;
+    return 0;
+}
