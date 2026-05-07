19c152bbbc7c83b9f0c69c9c2fc92db7eb0cae11
Sumana Panda <manjupanda42@gmail.com>
2026-05-07 19:53:34 +0530

    added even or odd program

---
+++ if else/even-odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+
+    if(num%2==0) {
+        cout<<"Even";
+    }
+    else if(num%2!=0) {
+        cout<<"Odd";
+    }
+    return 0;
+}
