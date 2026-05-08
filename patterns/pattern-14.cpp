8237c6afd0a86c0acab521b0f9ffb4ace0c72a01
Sumana Panda <manjupanda42@gmail.com>
2026-05-08 16:18:44 +0530

    added pattern fourteen program

---
+++ patterns/pattern-14.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+
+    int row=1;
+    char alph='A';
+    while(row<=n) {
+        int column=1;
+        while(column<=row) {
+            char count=alph+row-1;
+            cout<<count;
+            column=column+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+    return 0;
+}
