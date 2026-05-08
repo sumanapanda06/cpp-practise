6ce92058f50aa719db1be576688232b173d456bc
Sumana Panda <manjupanda42@gmail.com>
2026-05-08 15:18:01 +0530

    added pattern six program

---
+++ patterns/pattern-6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+
+    int row=1;
+    while(row<=n) {
+        int column=1;
+        while(column<=n) {
+            cout<<n-column+1;
+            column=column+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+    return 0;
+}
