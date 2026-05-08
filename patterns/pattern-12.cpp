058af35ee852e18a95fc07b0e426d2386ecc81c5
Sumana Panda <manjupanda42@gmail.com>
2026-05-08 16:02:24 +0530

    added pattern twelve program

---
+++ patterns/pattern-12.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+
+    int row=1;
+    char alph='A';
+    while(row<=n) {
+        int column=1;
+        while(column<=n) {
+            cout<<alph;
+            alph=alph+1;
+            column=column+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+    return 0;
+}
