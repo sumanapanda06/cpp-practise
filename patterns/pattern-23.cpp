55a63afcf5f35e577eeaf03996ce68e1e836849a
Sumana Panda <manjupanda42@gmail.com>
2026-05-09 18:22:20 +0530

    added pattern twentythree program

---
+++ patterns/pattern-23.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+
+    int row=1;
+    while(row<=n) {
+        int space=n-row;
+        while(space) {
+            cout<<" ";
+            space=space-1;
+        }
+        int column=1;
+        while(column<=row) {
+            cout<<row;
+            column=column+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+    return 0;
+}
