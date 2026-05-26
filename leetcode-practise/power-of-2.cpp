10437dd336eeb3026abad13a11eb763bf465d855
Sumana Panda <manjupanda42@gmail.com>
2026-05-26 21:23:05 +0530

    added power of two program

--- leetcode-practise/power-of-2.cpp
+++ leetcode-practise/power-of-2.cpp
@@ -20,3 +20,27 @@ int main() {
     }
     return 0;
 }
+
+//another method// 
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+    bool ans=false;
+
+    for(int i=0;i<=31;i++) {
+        if(n==(1<<i)) {
+            ans=true;
+            break;
+        }
+    }
+    if(ans) {
+        cout<<"yes";
+    }
+    else {
+        cout<<"No";
+    }
+    return 0;
+}
