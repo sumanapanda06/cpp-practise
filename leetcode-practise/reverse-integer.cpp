ffc50a4869b58b7124b531e398d3ff1ce429ba7e
Sumana Panda <manjupanda42@gmail.com>
2026-05-29 12:03:49 +0530

    added reverse of an integer program

--- leetcode-practise/reverse-integer.cpp
+++ leetcode-practise/reverse-integer.cpp
@@ -5,6 +5,11 @@ int main() {
     cout<<"Enter value of n: ";
     cin>>n;
 
+    if(n==0) {
+        cout<<"0";
+        return 0;
+    }
+
     while(n!=0) {
         int digit=n%10;
         cout<<digit;
