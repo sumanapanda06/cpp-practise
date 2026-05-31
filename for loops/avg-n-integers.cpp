4abfcdfa9a2c4914d20aa2dc4dbe02c7b6e9339c
Sumana Panda <manjupanda42@gmail.com>
2026-05-31 16:28:20 +0530

    added avg of n numbers program

---
+++ for loops/avg-n-integers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+
+    int avg=0;
+    for(int i=0; i<=n; i++) {
+        avg+=i;
+    }
+    float res=avg/n;
+    cout<<res;
+}
