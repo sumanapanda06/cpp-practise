af3929fd49e3bcc793631ae45e9911e6708a1545
Sumana Panda <manjupanda42@gmail.com>
2026-06-01 20:45:26 +0530

    added size of array program

---
+++ arrays/size-array-functions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int sizeArray(int arr[], int size) {
+    return size;
+}
+int main() {
+    int array[12];
+    int res=sizeArray(array,12);
+    cout<<res;
+    return 0;
+}
