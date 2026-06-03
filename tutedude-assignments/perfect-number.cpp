22fcbc4f1cfd79ed8b921978fa209b3a3c536bb6
Sumana Panda <manjupanda42@gmail.com>
2026-06-03 21:14:23 +0530

    added perfect number program

---
+++ tutedude-assignments/perfect-number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main () {
+    for(int i=1; i<=500; i++) {
+        int sum=0;
+        for(int j=1; j<i; j++) {
+            if (i%j==0) {
+                sum+=j;
+            }
+        }
+        if(sum==i) {
+            cout<<i<<" ";
+        }
+    }
+}
