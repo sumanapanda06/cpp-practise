139c7af9bd6467e707ad5d1fcbec1aec73825384
Sumana Panda <manjupanda42@gmail.com>
2026-06-03 23:44:05 +0530

    added gcd program

---
+++ tutedude-assignments/GCD.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int gcd(int num1, int num2) {
+    int value;
+    while(num1%num2!=0) {
+        value=num1%num2;
+        num1=num2;
+        num2=value;
+    }
+    return num2;
+}
+int main() {
+    int num1;
+    cout<<"Enter first number: ";
+    cin>>num1;
+
+    int num2;
+    cout<<"Enter second number: ";
+    cin>>num2;
+
+    if(num1>num2) {
+        int GCD=gcd(num1,num2);
+        cout<<"GCD: "<<GCD;
+    }
+    else {
+        int GCD=gcd(num2,num1);
+            cout<<"GCD: "<<GCD;
+    }
+}
